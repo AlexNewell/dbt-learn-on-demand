00f9224396d6b7a84f89aee9fae2f1cd77003702
Alex Newell <alexandernewell2021@u.northwestern.edu>
2021-11-14 18:12:28 +0000

    finish models tutorial section

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,7 @@
+select orderid as order_id        
+, paymentmethod
+, status
+-- amount is stored in cents, convert it to dollars
+, amount / 100 as amount
+, created 
+from dbt-tutorial.stripe.payment
